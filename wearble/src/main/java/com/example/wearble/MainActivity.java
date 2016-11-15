fb4734d980749becf8f0d955816fe03d63d507c5
Samir Antonio <samir.ma@gmail.com>
2016-11-14 23:34:32 -0200

    time goal

--- wearble/src/main/java/com/example/wearble/MainActivity.java
+++ wearble/src/main/java/com/example/wearble/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.wearble;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.wearable.view.WatchViewStub;
+import android.text.format.DateUtils;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -13,12 +19,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+        mTextView = (TextView) findViewById(R.id.text);
+
+        final String text = DateUtils.formatDateTime(this, new Date().getTime(), SimpleDateFormat.YEAR_FIELD);
+        mTextView.setText(text);
+
+
     }
 }
